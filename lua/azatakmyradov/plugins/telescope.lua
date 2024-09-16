22cb52a60ab573909da1bcc437caf15a7fd9fcbf
Azat Akmyradov <azat@rebdev.com>
2024-09-16 10:46:44 -0400

    add ignore files to file search

--- lua/azatakmyradov/plugins/telescope.lua
+++ lua/azatakmyradov/plugins/telescope.lua
@@ -55,12 +55,27 @@ return { -- Fuzzy Finder (files, lsp, etc)
 
     -- See `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
+
+    -- ignore this folders
+    local ignore_patterns = { 'node_modules', 'vendor', '.git', 'storage/clockwork' }
+
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>f', function()
+      builtin.find_files {
+        file_ignore_patterns = ignore_patterns,
+        prompt_title = '[S]earch [F]iles',
+      }
+    end)
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+    vim.keymap.set('n', '<leader>sg', function()
+      builtin.live_grep {
+        file_ignore_patterns = ignore_patterns,
+        prompt_title = '[S]earch by [G]rep',
+      }
+    end)
+    vim.keymap.set('n', '<leader>G', builtin.live_grep, { desc = '[S]earch by [G]rep (global)' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
